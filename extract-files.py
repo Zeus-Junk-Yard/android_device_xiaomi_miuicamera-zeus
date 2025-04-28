0d7c1429df94464d0c1410c8845dc3372f2d81d6
Anush02198 <anush.4376@gmail.com>
2025-04-28 20:38:07 +0530

    Revert "Adjust for cupid"

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-cupid',
+    'device/xiaomi/miuicamera-zeus',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-cupid',
+    'miuicamera-zeus',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
